102e865cb857d1920e0bc62e4da97d5d29dee71f
Matthew Alvidrez <mateo75602@gmail.com>
2023-04-03 16:27:49 -0500

    module 2 update

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // js file for module 2 assignment 
+(function (){
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController);
+
+
+})();
