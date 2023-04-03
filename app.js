7f5b09492c497fc216b0b268feaf29845627b2bb
Matthew Alvidrez <mateo75602@gmail.com>
2023-04-03 16:56:17 -0500

    module 2 update

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // js file for module 2 assignment 
-(function (){
+(function () {
     'use strict';
 
     angular.module('ShoppingListCheckOff', [])
     .controller('ToBuyController', ToBuyController)
-    .controller('AlreadyBoughtController', AlreadyBoughtController);
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController($scope) {
+
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController = []
 
 })();
